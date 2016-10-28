a505bb5e35bb0a3b93347668b63c8be00b618dcd
Ryan Fredlund <rfredlund13@gmail.com>
2016-10-27 19:14:25 -0500

    Added switch code

--- MunchiSketch/MunchiSketch.ino
+++ MunchiSketch/MunchiSketch.ino
@@ -1,6 +1,7 @@
 //Munchi Code - bookdude13
 
 #define RELAY 12
+#define SWITCH 7
 #define DELAY 500
 
 double temperature = 0.0;
@@ -8,12 +9,19 @@ double temperature = 0.0;
 void setup()
 {
   pinMode(RELAY, OUTPUT);
+  pinMode(SWITCH, INPUT);
 }
 
 void loop()
 {
-//  digitalWrite(RELAY, LOW); // LOW means on 
-  digitalWrite(RELAY, HIGH); // HIGH means off
+  if (digitalRead(SWITCH)==HIGH)
+  {
+    digitalWrite(RELAY, LOW); // LOW means on
+  }
+  else
+  {
+    digitalWrite(RELAY, HIGH); // HIGH means off
+  }
   temperature = read_temp();
   delay(DELAY);
 }
