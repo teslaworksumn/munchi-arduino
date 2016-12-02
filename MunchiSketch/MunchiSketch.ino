fce88ba8e073b49631ee24c004d4e39be70e6804
Ryan Fredlund <rfredlund13@gmail.com>
2016-12-01 19:37:31 -0600

    Start of thermistor code

--- MunchiSketch/MunchiSketch.ino
+++ MunchiSketch/MunchiSketch.ino
@@ -2,6 +2,7 @@
 
 #define RELAY 12
 #define SWITCH 7
+#define THERM A0
 #define DELAY 500
 
 double temperature = 0.0;
@@ -10,25 +11,29 @@ void setup()
 {
   pinMode(RELAY, OUTPUT);
   pinMode(SWITCH, INPUT);
+  Serial.begin(9600);
 }
 
 void loop()
 {
-  if (digitalRead(SWITCH)==HIGH)
-  {
-    digitalWrite(RELAY, LOW); // LOW means on
-  }
-  else
-  {
-    digitalWrite(RELAY, HIGH); // HIGH means off
-  }
+  // if (digitalRead(SWITCH)==HIGH)
+  // {
+  //   digitalWrite(RELAY, LOW); // LOW means on
+  // }
+  // else
+  // {
+  //   digitalWrite(RELAY, HIGH); // HIGH means off
+  // }
   temperature = read_temp();
+  Serial.println(temperature);
   delay(DELAY);
 }
 
 double read_temp()
 {
-    return 0.0;
+  double temp = analogRead(THERM);
+  return (1.0 / ((3.3 / 1023.0) * temp));
 }
+
 
 
