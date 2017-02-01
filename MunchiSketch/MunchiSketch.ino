be00526f7b600eb1a995d277f516c24f7cb91ee6
Nick Knudsen <nickknudsen3@gmail.com>
2017-02-01 17:28:31 -0600

    Added Steinhart-Hart Equation
    Added the Steinhart-Hart equation to convert resistance to temperature.

--- MunchiSketch/MunchiSketch.ino
+++ MunchiSketch/MunchiSketch.ino
@@ -5,6 +5,10 @@
 #define THERM A0
 #define DELAY 500
 
+#define THERMISTORVALUE 100000
+#define SERIESRESISTOR 47000
+#define BCOEFFICIENT 4072
+
 double temperature = 0.0;
 
 void setup()
@@ -24,15 +28,38 @@ void loop()
   // {
   //   digitalWrite(RELAY, HIGH); // HIGH means off
   // }
-  temperature = read_temp();
-  Serial.println(temperature);
+ 
+  //temperature = read_temp();
+  //Serial.println(temperature);
+
+  read_temp();
   delay(DELAY);
 }
 
 double read_temp()
 {
-  double temp = analogRead(THERM);
-  return (1.0 / ((3.3 / 1023.0) * temp));
+  double reading = analogRead(THERM);
+  Serial.print("Analog Reading: ");
+  Serial.println(reading);
+
+  reading = (1023 / reading) -1;
+  reading = SERIESRESISTOR / reading;
+  Serial.print("Thermistor Resistance: ");
+  Serial.println(reading);
+
+  double temp = reading / THERMISTORVALUE;
+  temp = log(temp);
+  temp /= BCOEFFICIENT;
+  temp += 1.0 / (THERMISTORVALUE + 273.15);
+  temp = 1.0 / temp;
+  temp -= 273.15;
+
+  Serial.print("Temperature ");
+  Serial.print(temp);
+  Serial.println(" *C");
+
+  
+  //return (1.0 / ((3.3 / 1023.0) * temp));
 }
 
 
