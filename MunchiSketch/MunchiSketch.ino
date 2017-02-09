c13cf9f19ef5251a4624156cc08191cdb23890a5
Ryan Fredlund <rfredlund13@gmail.com>
2017-02-09 11:44:32 -0600

    Rearranged and renamed variables

--- MunchiSketch/MunchiSketch.ino
+++ MunchiSketch/MunchiSketch.ino
@@ -31,23 +31,23 @@ void loop()
  
   //temperature = read_temp();
   //Serial.println(temperature);
-
-  read_temp();
-  delay(DELAY);
-}
-
-double read_temp()
-{
+  
   double reading = analogRead(THERM);
   Serial.print("Analog Reading: ");
   Serial.println(reading);
 
-  reading = (1023 / reading) -1;
+  reading = (1023 / reading) - 1;
   reading = SERIESRESISTOR / reading;
   Serial.print("Thermistor Resistance: ");
   Serial.println(reading);
 
-  double temp = reading / THERMISTORVALUE;
+  read_temp(reading);
+  delay(DELAY);
+}
+
+void read_temp(double resistance)
+{
+  double temp = resistance / THERMISTORVALUE;
   temp = log(temp);
   temp /= BCOEFFICIENT;
   temp += 1.0 / (THERMISTORVALUE + 273.15);
@@ -57,9 +57,8 @@ double read_temp()
   Serial.print("Temperature ");
   Serial.print(temp);
   Serial.println(" *C");
-
   
-  //return (1.0 / ((3.3 / 1023.0) * temp));
+  temperature = temp;
 }
 
 
